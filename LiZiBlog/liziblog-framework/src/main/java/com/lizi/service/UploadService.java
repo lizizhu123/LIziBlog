42bba2e1f5de47b1aee3e14e52d9a91bd564c55a
Li Ziheng <114842565+lizizhu123@users.noreply.github.com>
2024-01-11 10:16:06 +0800

    Delete LiZiBlog directory

--- LiZiBlog/liziblog-framework/src/main/java/com/lizi/service/UploadService.java
+++
@@ -1,8 +0,0 @@
-package com.lizi.service;
-
-import com.lizi.domain.ResponseResult;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface UploadService {
-    ResponseResult uploadImg(MultipartFile img);
-}
